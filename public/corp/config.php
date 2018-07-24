8f670fe9348991a795ec232f4c4904b039b43392
牛牛 <1032380143@qq.com>
2018-07-24 10:55:48 +0800

    删除目录public

--- public/corp/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DIR_ROOT', dirname(__FILE__).'/');
-define("OAPI_HOST", "https://oapi.dingtalk.com");
-
-define("CORPID", "dingb8f2e19299cab872");
-define("SECRET", "uM21n9Zp4YXZm9MUCJeHo7YVyz3SelJ02X5PmnDFN7fZ8gef8AAQ2NSodDAvNar4");
-define("AGENTID", "39806381");//必填，在创建微应用的时候会分配
-
--- public/corp/filecache.php
+++
@@ -1,1 +0,0 @@
-<?php exit();?>{"corp_access_token":{"corp_access_token":"b9df64a549f53403ae5f14453ce5a517","expire_time":1473072615,"create_time":1473065615},"js_ticket":{"js_ticket":"B0ZwruTsrMdUk5syOvHpVANYQiOOrFeXS2NN17Z6RcbuuVpWiWvntg7elmZgk8IIOTJqOXiuwdPfOCs4lVEBY9","expire_time":1473072615,"create_time":1473065615}}
