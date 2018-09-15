a1cf303c6e6b95cbef2d1509f5015e8d910790b2
1032380143 <1032380143@qq.com>
2018-09-15 14:47:27 +0800

    报销添加终极审批字段

---
+++ database/migrations/2018_09_15_143912_add_reimbursements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddReimbursementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reimbursements', function (Blueprint $table) {
+            $table->char('finance_approved_sn',6)->index()->default('')->comment('终极boss审批员工工号');
+            $table->char('finance_approved_name',10)->default('')->comment('终极boss审批员工');
+            $table->dateTime('finance_approved_at')->nullable()->comment('终极boss审批时间');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reimbursements', function (Blueprint $table) {
+            $table->dropColumn([
+               'finance_approved_sn',
+               'finance_approved_name',
+               'finance_approved_at',
+            ]);
+        });
+    }
+}
