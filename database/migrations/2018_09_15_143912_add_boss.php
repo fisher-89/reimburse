2d3f4f0ca303a5a790409ebe57795f8a1274a98a
Fisher <544473461@qq.com>
2018-09-25 17:07:20 +0800

    fix:两个迁移类同名，无法回滚

--- database/migrations/2018_09_15_143912_add_boss.php
+++ database/migrations/2018_09_15_143912_add_boss.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddReimbursementsTable extends Migration
+class AddBoss extends Migration
 {
     /**
      * Run the migrations.
